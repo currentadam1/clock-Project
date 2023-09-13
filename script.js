1e8ada2d531cfca2ecc2e1d1d16a03afbd28747b
Adam <adamadeniji2004@gmail.com>
2023-09-13 14:10:38 +0100

    seventh commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const secondsHand = document.getElementById('seconds-hand');
-const minutesHand = document.getElementById('minutes-hand');
-const hoursHand = document.getElementById('hours-hand');
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
 
 function getTime() {
   const now = new Date();
@@ -8,10 +8,11 @@ function getTime() {
   const minutes = now.getMinutes();
   const hours = now.getHours();
   const timeInterval = 6;
-
-  secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) +'deg)'
-  minutesHand.style.transform = 'rotate(' + (minutes* timeInterval + seconds / 10) +'deg)'
-  hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) +'deg)'
- }
-
- setInterval(getTime, 100)
+  // giving the hands their functions
+  secondsHand.style.transform = "rotate(" + seconds * timeInterval + "deg)";
+  minutesHand.style.transform =
+    "rotate(" + (minutes * timeInterval + seconds / 10) + "deg)";
+  hoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)";
+}
+// calling the function with the time frame
+setInterval(getTime, 100);
